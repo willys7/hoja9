44925ddbab9c1b81cf1b298c9819650ba5bbe7ae
willys7 <willys_f7@hotmail.com>
2014-10-26 23:24:39 -0600

    Update Hoja9.java

--- src/hoja9/Hoja9.java
+++ src/hoja9/Hoja9.java
@@ -14,83 +14,85 @@ public class Hoja9 {
 
     public static void main(String[] args){
         
-        GrafoFloyd matriz = new GrafoFloyd(); // Matriz de adyacencia
-        matriz.caminoCorto(); // Algoritmo de Floyd para encontrar el camino mas corto entre todos los nodos
-        int op1=1; // Seleccion del usuario para realizar
-        int op2; // Seleccion del usuario para la pregunta 3
-        Scanner scan = new Scanner(System.in); //Entrada por teclado
+        AlgoDeFloyd GrafoCiu = new AlgoDeFloyd(); // Aqui creamos la matriz de adyacencia del grefo que leemos del archivo de texto
+        GrafoCiu.masCorto(); // Le aplicamos el algoritmo de Floyd para encontrar el camino mas corto
+        int op1=1; // El usiarios debe elegir una opcion
+        int op2; // la pregunta de modificacion del grafo necesita una segunda eleccion
+        Scanner scan = new Scanner(System.in); 
 
-        // Realizar ciclo mientras la respuesta del usuario no sea 4
-        while(op1!=4){       
+
+        while(op1!=4){       //Esta en un while esperando la opcion que el usuario elija hasta que sea 4 que es para salir 
             System.out.println("1. Buscar ruta mas corta entre dos ciudades ");
             System.out.println("2. Nombre de la ciudad en el centro del grafo");
-            System.out.println("3. Modificar grafo");
-            System.out.println("4. Finalizar programa");
+            System.out.println("3. CAmbiar el grafo");
+            System.out.println("4. Salir del programa");
             op1 = scan.nextInt();
-            // Desplegar la matriz
-            System.out.println("\nMatriz de adyacencia");
-            matriz.inter.mostrar();
             
-            // Si la respuesta es 1, preguntar por las ciudades y desplegar resultados
+            
+            //Imprime la matriz de adyacendia del grafo respectivo
+            System.out.println("\nMatriz de adyacencia");
+            GrafoCiu.inter.mostrar();
+            
+            
             if(op1==1){
-                matriz.caminoCorto(); // Camino mas corto entre todas las ciudades
-                System.out.println("Ingrese el nombre de la ciudad de salida");
+                GrafoCiu.masCorto(); // Aplicamos Floyd para el camino mas corto entre 2 nodos
+                System.out.println("Nombre de la ciudad de inicial");
                 String ciudad1 = scan.next(); 
-                System.out.println("Ingrese el nombre de la ciudad de destino");
+                System.out.println("A donde desea llegar");
                 String ciudad2 = scan.next();
-                // Si las ciudades se encuentran en la matriz, entonces desplegar la distancia minima y la ruta completa
-                if(matriz.inter.contains(ciudad1)&&matriz.inter.contains(ciudad2)){
-                    System.out.println("\nLa distancia minima es: "+matriz.inter.getVertice(ciudad1, ciudad2)+".");
-                    if(matriz.inter.getVertice(ciudad1, ciudad2)!=10000){
-                        System.out.print("La ruta es: "+ciudad1);
-                        matriz.mostrarIntermedias(matriz.inter.getIndex(ciudad1), matriz.inter.getIndex(ciudad2));
+                 if(GrafoCiu.inter.contains(ciudad1)&&GrafoCiu.inter.contains(ciudad2)){
+                    System.out.println("\nLa distancia minima encontrada es: "+GrafoCiu.inter.getVertice(ciudad1, ciudad2)+".");
+                    if(GrafoCiu.inter.getVertice(ciudad1, ciudad2)!=10000){
+                        System.out.print("EL camino que debe seguir es: "+ciudad1);
+                        GrafoCiu.CiudadesMedias(GrafoCiu.inter.getIndex(ciudad1), GrafoCiu.inter.getIndex(ciudad2));
                         System.out.println(", "+ciudad2);
                     }
                 }
             }
             
-            // Si la respuesta es 2, desplegar el centro de grafo
+            // Si el usuario eligio el 2, se le muestra el centro del grafo
             else if(op1==2){
-                matriz.centroGrafo();
+                GrafoCiu.centroDeGraf();
             }
             
-            // Si la respuesta es 3, preguntar cual es el cambio a realizar y ejecutarlo
+            // Si elije la opcion se procede apreguntar que cambio quiere y se realiza
             else if(op1==3){
-                System.out.println("1. Hay interrupcion de trafico entre un par de ciudades");
-                System.out.println("2. Establecer nueva conexion");
+                System.out.println("1. Se encuentran problemas en la carretera entre 2 ciudades");
+                System.out.println("2. Nueva carretera entre ciudades");
                 op2 = scan.nextInt();
-                // Si la respuesta es 1, preguntar por el nombre de las ciudades y colocar un numero muy grande
-                // en la matriz de adyacencia para indicar que no hay conexion
+ 
                 if(op2==1){
-                    System.out.println("Ingrese el nombre de la ciudad de salida");
+                    System.out.println("Nombre ciudad inicial");
                     String ciudad1 = scan.next();
-                    System.out.println("Ingrese el nombre de la ciudad de destino");
+                    System.out.println("A donde desea llegar");
                     String ciudad2 = scan.next();
-                    if(matriz.inter.contains(ciudad1)&&matriz.inter.contains(ciudad2)){
-                        matriz.inter.agregarVertice(ciudad1, ciudad2, 10000);
+                    if(GrafoCiu.inter.contains(ciudad1)&&GrafoCiu.inter.contains(ciudad2)){
+                        GrafoCiu.inter.agregarVertice(ciudad1, ciudad2, 10000);
                     }
                 }
-                // Si la respuesta es 2, preguntar por el nombre de las ciudades y agregar nueva distancia.
+               
                 else if(op2==2){
-                    System.out.println("Ingrese el nombre de la ciudad de salida");
+                    System.out.println("nombre ciudad inicial");
                     String ciudad1 = scan.next();
-                    System.out.println("Ingrese el nombre de la ciudad de destino");
+                    System.out.println("A donde quiere llegar");
                     String ciudad2 = scan.next();
-                    System.out.println("Ingrese la distancia entre las ciudades");
+                    System.out.println("Cual es la nuevo distancia entre las ciudades");
                     int distancia = scan.nextInt();
-                    // Si las ciudades ya existen, cambiar el valor
-                    if(matriz.inter.contains(ciudad1)&&matriz.inter.contains(ciudad2)){
-                        matriz.inter.agregarVertice(ciudad1, ciudad2, distancia);
+                    // En el caso de que las ciudades ya existirean solo se remplaza el valor de la distancia
+                    if(GrafoCiu.inter.contains(ciudad1)&&GrafoCiu.inter.contains(ciudad2)){
+                        GrafoCiu.inter.agregarVertice(ciudad1, ciudad2, distancia);
                     }
-                    // Si las ciudades no existen, agregarlas a la matriz
+                    // En el caso de que la ciudades aun no esten el grafo
+                    // se deben meter a la matriz del grafo
                     else{
-                        matriz.inter.agregar(ciudad1);
-                        matriz.inter.agregar(ciudad2);
-                        matriz.inter.agregarVertice(ciudad1, ciudad2, distancia);
+                        GrafoCiu.inter.agregar(ciudad1);
+                        GrafoCiu.inter.agregar(ciudad2);
+                        GrafoCiu.inter.agregarVertice(ciudad1, ciudad2, distancia);
                     }
                 }
-                // Recalcular las rutas mas cortas
-                matriz.caminoCorto();
+                // Luego de que se realizaron cualquiera de las modificaciones 
+                // se lleva a cabo el proceso de volver a aplicar Floy para el camino mas corto
+                GrafoCiu.masCorto();
             }
 
         }
